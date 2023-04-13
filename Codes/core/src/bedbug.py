8ef33c72d1b8ee32777fb58c00f901a2e5a2a46c
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-04-13 19:38:38 +0330

    Complete "get_group" function

--- Codes/core/src/bedbug.py
+++ Codes/core/src/bedbug.py
@@ -1,15 +1,22 @@
 from models.group import Group
+import const
+from errors.group_not_found_error import GroupNotFoundError
 
 _groups: dict[str, Group] = {}
 
 
-def create_group(name: str) -> None:
+def create_group(name: str) -> Group:
     group = Group(name)
     _groups[name] = group
+    return group
 
 
-def get_group(name: str) -> Group:
-    pass
+def get_group(name: str = const.DEFAULT_GROUP_NAME) -> Group:
+    if name not in _groups:
+        if name == const.DEFAULT_GROUP_NAME:
+            return create_group(const.DEFAULT_GROUP_NAME)
+        raise GroupNotFoundError
+    return _groups[name]
 
 
 def add_var(var) -> None:
