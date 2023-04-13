0e802f50fa88a54987ed39fb8bf12fdf3e8b10c7
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-04-13 19:23:21 +0330

    Create bedbug.py

---
+++ Codes/core/src/bedbug.py
@@ -0,0 +1,25 @@
+from models.group import Group
+
+
+def create_group(name: str) -> None:
+    pass
+
+
+def get_group(name: str) -> Group:
+    pass
+
+
+def add_var(var) -> None:
+    pass
+
+
+def plot() -> None:
+    pass
+
+
+def dump_json(filename: str) -> None:
+    pass
+
+
+def import_json(filename: str) -> None:
+    pass
