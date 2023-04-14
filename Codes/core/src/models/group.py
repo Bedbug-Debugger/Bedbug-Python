357e43b46c78276bfef40cad7063c80475373da9
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-04-14 21:33:48 +0330

    Add support for single and dict versions of add_data
    with @overload

--- Codes/core/src/models/group.py
+++ Codes/core/src/models/group.py
@@ -1,3 +1,4 @@
+from typing import overload
 from .time_manager import time
 
 class Group:
@@ -6,17 +7,24 @@ class Group:
         self.name: str = name
         self.vars: dict[str, list] = {}
 
-    def add_data(self, label: str, data) -> None:
+    def add_data_single(self, label: str, data) -> None:
         if label not in self.vars:
             self.vars[label] = []
         self.vars[label].append((time.current_time, data))
         time.tick()
-    
-    def add_data(self, data_dict: dict) -> None:
+
+    def add_data_dict(self, data_dict: dict) -> None:
         time.pause()
         for label, data in data_dict.items():
-            self.add_data(label, data)
+            self.add_data_single(label, data)
         time.resume()
+    
+    @overload
+    def add_data(self, label: str, data) -> None:
+        self.add_data_single(label, data)
+    
+    def add_data(self, data_dict: dict) -> None:
+        self.add_data_dict(data_dict)
         
     def plot(self) -> None:
         pass
