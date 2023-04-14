0baec052fbdaad3dbc055741059d35a2dccdd8c3
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-04-14 21:35:31 +0330

    Add support for pyplot wave [incompleted]

--- Codes/core/src/plot/pyplot_plotting.py
+++ Codes/core/src/plot/pyplot_plotting.py
@@ -1,4 +1,20 @@
 import matplotlib.pyplot as plt
+from .. import (
+    bedbug as bd,
+    const
+)
+
 
 def plot():
-    pass
+    signals: list[tuple[str, str]] = []
+    for groupname in bd._groups.keys():
+        for varname in bd._groups[groupname].vars.keys():
+            signals.append((groupname, varname))
+    num_of_signals = len(signals)
+    ax1 = None
+    for signal in range(num_of_signals):
+        if signal == 0:
+            ax1 = plt.subplot(num_of_signals, 1, signal + 1)
+        else:
+            ax1 = plt.subplot(num_of_signals, 1, signal + 1, sharex=ax1)
+        plt.plot()
