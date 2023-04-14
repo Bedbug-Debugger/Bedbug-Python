54158ba57b0dd2dbd1e9be30e16112e518f79fd1
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-04-14 19:16:28 +0330

    Create time_manager.py

---
+++ Codes/core/src/models/time_manager.py
@@ -0,0 +1,32 @@
+from .. import const
+
+class time_manager:
+    
+	def __init__(self, name: str = const.DEFAULT_TIME_MANAGER_NAME):
+		self.name: str = name
+		self.current_time: int = 0
+		self.paused: bool = False
+	
+	def tick(self) -> None:
+		if not self.paused:
+			self.current_time += 1
+	
+	def pause(self) -> None:
+		self.paused = True
+	
+	def resume(self) -> None:
+		self.paused = False
+		# For correct behavior after a pause, a tick is needed.
+		# Example:
+		#	import bedbug as bd
+		#	a, b = 0, 0
+		#	bd.time.pause()
+		#	bd.add_data("a", a)		# adds (0, a), keeps current_time = 0
+		#	bd.add_data("b", b)		# adds (0, b), keeps current_time = 0
+		#	bd.time.resume()		# lets current_time = 1
+		#	a = 12
+		#	bd.add_data("a", a)		# adds (1, a) since current_time is 1, then lets current_time = 2
+		self.tick()
+
+
+time = time_manager()
