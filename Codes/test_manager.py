00ad35de9c4eeaf8f15dede55bd40ccc976e9361
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-04-14 16:01:19 +0330

    Add pyplot_int_vars_test1 runnable

--- Codes/test_manager.py
+++ Codes/test_manager.py
@@ -1,11 +1,12 @@
-import tests.tests_import
+from tests import tests_import
+import core.plot.pyplot_plotting
 
 # def run_test_pyplot_int_vars_test1():
     # pass
 
 
 def main():
-    tests.pyplot_int_vars.test1.run()
+    tests_import.pyplot_int_vars_test1.run()
 
 if __name__ == '__main__':
     main()
