d5d76aa2b15e7612baa9b39eebbe6c211d6ba497
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-04-14 19:17:16 +0330

    Update group.py to support time management

--- Codes/core/src/models/group.py
+++ Codes/core/src/models/group.py
@@ -1,13 +1,22 @@
+from time_manager import time
+
 class Group:
 
     def __init__(self, name: str):
         self.name: str = name
         self.vars: dict[str, list] = {}
 
-    def add_data(self, data, label: str) -> None:
+    def add_data(self, label: str, data) -> None:
         if label not in self.vars:
             self.vars[label] = []
-        self.vars[label].append(data)
-
+        self.vars[label].append((time.current_time, data))
+        time.tick()
+    
+    def add_data(self, data_dict: dict) -> None:
+        time.pause()
+        for label, data in data_dict.items():
+            self.add_data(label, data)
+        time.resume()
+        
     def plot(self) -> None:
         pass
