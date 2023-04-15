41e15841d3073179ee72739cc99fa67ada0c5961
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-04-15 15:51:13 +0330

    Update plot in pyplot_plotting.py [not debugged]
    - Completed function using bd.time
    - Changed some namings to snake_case

--- Codes/core/src/plot/pyplot_plotting.py
+++ Codes/core/src/plot/pyplot_plotting.py
@@ -13,14 +13,26 @@ def get_signal_name(groupname, varname):
 
 def plot():
     signals: list[tuple[str, str]] = []
-    for groupname in bd._groups.keys():
-        for varname in bd._groups[groupname].vars.keys():
-            signals.append((groupname, varname))
+    for group_name in bd._groups.keys():
+        for var_name in bd.get_group(group_name).vars.keys():
+            signals.append((group_name, var_name))
     num_of_signals: int = len(signals)
+    
     ax1: plt.Axes = None
+    t = range(bd.time.current_time)
     for signal in range(num_of_signals):
+        group_name = signals[signal][0]
+        var_name = signals[signal][1]
+        signal_name = get_signal_name(group_name, var_name)
+        group = bd.get_group(group_name)
+        values = group.vars[var_name]
+
         if signal == 0:
             ax1 = plt.subplot(num_of_signals, 1, signal + 1)
         else:
-            ax1 = plt.subplot(num_of_signals, 1, signal + 1, sharex=ax1)
-        plt.plot()
+            plt.subplot(num_of_signals, 1, signal + 1, sharex=ax1)
+        
+        plt.plot(t, values)
+        plt.ylabel(signal_name, rotation = 0)
+    
+    plt.show()
