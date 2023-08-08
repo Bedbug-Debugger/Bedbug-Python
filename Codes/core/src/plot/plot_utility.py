7193fb99a1677bb7369ce432708bf69dbda6bce9
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-08 16:30:28 +0330

    Use plt.step + clean code
    * Used plt.step instead of plt.plot, for more meaningful plots.
    * Introduced a number of plotting utility functions for cleaner code.
    * Removed get_values_from_dict function, since all functions now work with one main time_tick list extracted from all considered signals.
    * Rewrote the main plotting function based on the new utility functions.
    * Commented out old code, just in case.

--- Codes/core/src/plot/plot_utility.py
+++ Codes/core/src/plot/plot_utility.py
@@ -1,5 +1,11 @@
+from typing import Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 from .const import PlotConst
 from .. import const
+from .. import bedbug as bd
 
 
 def get_signal_name(groupname: str, varname: str) -> str:
@@ -13,32 +19,74 @@ def get_signal_name(groupname: str, varname: str) -> str:
         return varname
     return f"{groupname}/{varname}"
 
+def get_time_ticks(signals: list[tuple[str, str]]) -> list:
+    """
+    Find all time ticks when at least one data was added.
+    :param signals: List of all signals to search in
+    :type signals: list[tuple[str, str]]
+    :return: List of all extracted time ticks, in ascending order
+    :rtype: list
+    """
+    time_ticks = set()  # set is used to prevent adding duplicate time ticks
+    for signal in signals:
+        group_name = signal[0]
+        var_name = signal[1]
+        group = bd.get_group(group_name)
+        time_value_dict = group.vars[var_name]
+        for time_tick in time_value_dict:
+            time_ticks.add(time_tick)
+    time_ticks = list(time_ticks)
+    time_ticks.sort()
+    return time_ticks
 
-def get_values_from_dict(time_value_dict: dict, num_of_time_ticks: int) -> list:
-    """
-    Return the list of values of a variable in all sampling time ticks, based on the variable's value dictionary.
-    :param time_value_dict: the value dictionary of the variable, in the form of {time_tick: value}
-    :param num_of_time_ticks: the number of time ticks until calling this function
-    :return: list of values of the variable
-    """
-    # Initialize values
-    values = [PlotConst.EMPTY_VALUE] * num_of_time_ticks
-    # Fill registered values
-    for time_index, value in time_value_dict.items():
-        values[time_index] = value
-    # Fill empty values with last value, if any
-    value_exists_before = False
-    first_index_with_value = -1
-    for time_index in range(num_of_time_ticks):
-        if values[time_index] == PlotConst.EMPTY_VALUE:
-            if value_exists_before:
-                values[time_index] = values[time_index - 1]
-        else:
-            if not value_exists_before:
-                value_exists_before = True
-                first_index_with_value = time_index
-    # Fill initial empty values with first value
-    value_exists_before = False
-    for time_index in range(first_index_with_value):
-        values[time_index] = values[first_index_with_value]
-    return values
+def get_axes(axs: Union[plt.Axes, np.ndarray], num_of_signals: int, signal_num: int) -> plt.Axes:
+    if num_of_signals == 1:
+        return axs
+    return axs[signal_num]
+
+def plot_single_signal(ax: plt.Axes, time_ticks: list, signal_name: str, time_value_dict: dict):
+    time_list = []
+    value_list = []
+    # Add relevant (time_tick, value) points to time_list and value_list for plotting
+    for time_tick in time_ticks:
+        if time_tick in time_value_dict:
+            value = time_value_dict[time_tick]
+            time_list.append(time_tick)
+            value_list.append(value)
+    # Add sentinel element to time_list and value_list, needed for the tail of the plot
+    time_list.append(time_ticks[-1] + 1)    # time_ticks is referred to, so that all plots end in the same time tick
+    value_list.append(value_list[-1])
+    ax.step(time_list, value_list, 'b', where='post')
+    ax.plot(time_list[:-1], value_list[:-1], 'bo')
+    ax.set_ylabel(signal_name + 4*' ', rotation='horizontal')
+
+
+
+# def get_values_from_dict(time_value_dict: dict, num_of_time_ticks: int) -> list:
+#     """
+#     Return the list of values of a variable in all sampling time ticks, based on the variable's value dictionary.
+#     :param time_value_dict: the value dictionary of the variable, in the form of {time_tick: value}
+#     :param num_of_time_ticks: the number of time ticks until calling this function
+#     :return: list of values of the variable
+#     """
+#     # Initialize values
+#     values = [PlotConst.EMPTY_VALUE] * num_of_time_ticks
+#     # Fill registered values
+#     for time_index, value in time_value_dict.items():
+#         values[time_index] = value
+#     # Fill empty values with last value, if any
+#     value_exists_before = False
+#     first_index_with_value = -1
+#     for time_index in range(num_of_time_ticks):
+#         if values[time_index] == PlotConst.EMPTY_VALUE:
+#             if value_exists_before:
+#                 values[time_index] = values[time_index - 1]
+#         else:
+#             if not value_exists_before:
+#                 value_exists_before = True
+#                 first_index_with_value = time_index
+#     # Fill initial empty values with first value
+#     value_exists_before = False
+#     for time_index in range(first_index_with_value):
+#         values[time_index] = values[first_index_with_value]
+#     return values
--- Codes/core/src/plot/pyplot_plotting.py
+++ Codes/core/src/plot/pyplot_plotting.py
@@ -9,27 +9,52 @@ def plot(signals: list[tuple[str, str]]) -> None:
     :param signals: a list of (group name, variable name) pairs
     :return: None
     """
-    num_of_signals: int = len(signals)
-    ax1: plt.Axes = None
-    num_of_time_ticks = bd.time.current_time
-    t = range(num_of_time_ticks)
-    for signal in range(num_of_signals):
-        group_name = signals[signal][0]
-        var_name = signals[signal][1]
+    time_ticks = plot_utility.get_time_ticks(signals)
+    num_of_signals = len(signals)
+    fig, axs = plt.subplots(num_of_signals, 1, sharex=True)
+    # Draw signals in different subplots
+    for signal_num in range(num_of_signals):
+        signal = signals[signal_num]
+        group_name, var_name = signal
         signal_name = plot_utility.get_signal_name(group_name, var_name)
         group = bd.get_group(group_name)
         time_value_dict = group.vars[var_name]
-
-        values = plot_utility.get_values_from_dict(time_value_dict, num_of_time_ticks)
-
-        if signal == 0:
-            ax1 = plt.subplot(num_of_signals, 1, signal + 1)
-        else:
-            plt.subplot(num_of_signals, 1, signal + 1, sharex=ax1)
-
-        plt.plot(t, values, 'b')
-        for time_index, val in time_value_dict.items():
-            plt.plot(time_index, val, 'ro')
-        plt.ylabel(signal_name, rotation=0)
-
+        this_ax = plot_utility.get_axes(axs, num_of_signals, signal_num)
+        plot_utility.plot_single_signal(this_ax, time_ticks, signal_name, time_value_dict)
+    # Set time axis values
+    last_ax = plot_utility.get_axes(axs, num_of_signals, num_of_signals-1)
+    last_ax.set_xticks(time_ticks)
     plt.show()
+
+
+
+# def plot(signals: list[tuple[str, str]]) -> None:
+#     """
+#     Plot all variables in the input list with pyplot's GUI engine.
+#     :param signals: a list of (group name, variable name) pairs
+#     :return: None
+#     """
+#     num_of_signals: int = len(signals)
+#     ax1: plt.Axes = None
+#     num_of_time_ticks = bd.time.current_time
+#     t = range(num_of_time_ticks)
+#     for signal in range(num_of_signals):
+#         group_name = signals[signal][0]
+#         var_name = signals[signal][1]
+#         signal_name = plot_utility.get_signal_name(group_name, var_name)
+#         group = bd.get_group(group_name)
+#         time_value_dict = group.vars[var_name]
+
+#         values = plot_utility.get_values_from_dict(time_value_dict, num_of_time_ticks)
+
+#         if signal == 0:
+#             ax1 = plt.subplot(num_of_signals, 1, signal + 1)
+#         else:
+#             plt.subplot(num_of_signals, 1, signal + 1, sharex=ax1)
+
+#         plt.plot(t, values, 'b')
+#         for time_index, val in time_value_dict.items():
+#             plt.plot(time_index, val, 'ro')
+#         plt.ylabel(signal_name, rotation=0)
+
+#     plt.show()
