fd6680f3dc187692edf99bf62ef85b58fdcab038
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-09 12:24:19 +0330

    Add plot function for tkinter + docstring

--- Codes/core/src/plot/tkinter_plotting.py
+++ Codes/core/src/plot/tkinter_plotting.py
@@ -1,6 +1,11 @@
 from .tkinter.classes import TK_GUI, TkPlotterWindow
 
-def open_window():
+def plot(signals: list[tuple[str, str]]) -> None:
+    """
+    Plot all variables in the input list with tkinter GUI engine.
+    :param signals: a list of (group name, variable name) pairs
+    :type signals: list[tuple[str, str]]
+    """
     TK_GUI = TkPlotterWindow("tk GUI")
     TK_GUI.tk_element.mainloop()
     print('Meow')
