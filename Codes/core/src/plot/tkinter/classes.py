7937c3bd6f5d015e8fe8632e8556d3b1bea00db7
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-09 12:13:11 +0330

    Add TkPlotFrame + remove test code
    TkPlotFrame inherits from TkFrame and specifically manages a plot frame.

--- Codes/core/src/plot/tkinter/classes.py
+++ Codes/core/src/plot/tkinter/classes.py
@@ -9,37 +9,43 @@ class TkPlotterWindow:
 		self.tk_element = tk.Tk()
 		# Subwidgets
 		self.signal_name_frame = TkFrame(name='signal_name_frame', master=self.tk_element)
-		self.signal_plot_frame = TkFrame(name='signal_plot_frame', master=self.tk_element)
+		self.signal_plot_frame = TkPlotFrame(name='signal_plot_frame', master=self.tk_element)
 		# Event binding
 		self.tk_element.bind('<Key-Left>', self.handle_event)
 		self.tk_element.bind('<Key-Right>', self.handle_event)
-		# signal_name_frame
-		self.signal_name_frame.tk_element.pack()
-		self.label1 = tk.Label(master=self.signal_name_frame.tk_element, text="0")
-		self.label1.pack()
 
 	def handle_event(self, event):
-		print(f"{event.keysym=}")
-		num = int(self.label1['text'])
-		if event.keysym == 'Left':
-			num -= 1
-		elif event.keysym == 'Right':
-			num += 1
-		self.label1['text'] = f"{num}"
+		pass
 
 class TkFrame:
 
 	def __init__(self, name: str, master: Union[TkFrame, TkPlotterWindow], handled_events: list[str] = []):
 		self.name: str = name
 		self.master: Union[TkFrame, TkPlotterWindow] = master
-		self.tk_element = tk.Frame(
-			master=self.master
-		)
+		self.tk_element: tk.Frame = None
+		self.define_tk_element()
 		for handled_event in handled_events:
 			self.tk_element.bind(handled_event, self.handle_event)
 	
+	def define_tk_element(self):
+		self.tk_element = tk.Frame(
+			master=self.master
+		)
+		self.tk_element.pack()
+
 	def handle_event(self, event):
 		pass
+
+class TkPlotFrame(TkFrame):
+
+	def define_tk_element(self):
+		self.tk_element = tk.Frame(
+			master=self.master,
+			bg='#003366',
+			width=100,
+			height=100
+		)
+		self.tk_element.pack()
 
 
 
