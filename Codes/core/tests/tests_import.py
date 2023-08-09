1cc62ee1d703263e55e7c84fff3b702d81199f22
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-09 11:24:25 +0330

    Run tests

--- Codes/core/tests/tests_import.py
+++ Codes/core/tests/tests_import.py
@@ -1,3 +1,6 @@
 from .pyplot_int_vars import (
     test1 as pyplot_int_vars_test1
 )
+from .tkinter import (
+    test1 as tkinter_test1
+)
---
+++ Codes/core/tests/tkinter/test1.py
@@ -0,0 +1,12 @@
+from ...src import (
+    bedbug as bd
+)
+from ...src.plot import (
+    tkinter_plotting as tkp
+)
+
+def empty_window():
+    tkp.open_window()
+
+def run():
+    empty_window()
