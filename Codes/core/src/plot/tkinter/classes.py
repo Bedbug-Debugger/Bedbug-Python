6ed8997d38b391a5e844059ccab24986968c3231
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-09 11:58:34 +0330

    Change TkWindow name to TkPlotterWindow + misc
    Misc.:
    * Added comments
    * Tested event binding functionality

--- Codes/core/src/plot/tkinter/classes.py
+++ Codes/core/src/plot/tkinter/classes.py
@@ -2,23 +2,36 @@ from __future__ import annotations
 from typing import Union
 import tkinter as tk
 
-class TkWindow:
+class TkPlotterWindow:
     
 	def __init__(self, name: str):
 		self.name: str = name
 		self.tk_element = tk.Tk()
+		# Subwidgets
 		self.signal_name_frame = TkFrame(name='signal_name_frame', master=self.tk_element)
 		self.signal_plot_frame = TkFrame(name='signal_plot_frame', master=self.tk_element)
-		self.tk_element.bind('<Button-1>', self.handle_event)
-	
-	def handle_event(self):
-		print('Gotcha!')
+		# Event binding
+		self.tk_element.bind('<Key-Left>', self.handle_event)
+		self.tk_element.bind('<Key-Right>', self.handle_event)
+		# signal_name_frame
+		self.signal_name_frame.tk_element.pack()
+		self.label1 = tk.Label(master=self.signal_name_frame.tk_element, text="0")
+		self.label1.pack()
+
+	def handle_event(self, event):
+		print(f"{event.keysym=}")
+		num = int(self.label1['text'])
+		if event.keysym == 'Left':
+			num -= 1
+		elif event.keysym == 'Right':
+			num += 1
+		self.label1['text'] = f"{num}"
 
 class TkFrame:
 
-	def __init__(self, name: str, master: Union[TkFrame, TkWindow], handled_events: list[str] = []):
+	def __init__(self, name: str, master: Union[TkFrame, TkPlotterWindow], handled_events: list[str] = []):
 		self.name: str = name
-		self.master: Union[TkFrame, TkWindow] = master
+		self.master: Union[TkFrame, TkPlotterWindow] = master
 		self.tk_element = tk.Frame(
 			master=self.master
 		)
@@ -30,4 +43,4 @@ class TkFrame:
 
 
 
-TK_GUI: TkWindow = None
+TK_GUI: TkPlotterWindow = None
--- Codes/core/src/plot/tkinter_plotting.py
+++ Codes/core/src/plot/tkinter_plotting.py
@@ -1,6 +1,6 @@
-from .tkinter.classes import TK_GUI, TkWindow
+from .tkinter.classes import TK_GUI, TkPlotterWindow
 
 def open_window():
-    TK_GUI = TkWindow("tk GUI")
+    TK_GUI = TkPlotterWindow("tk GUI")
     TK_GUI.tk_element.mainloop()
     print('Meow')
