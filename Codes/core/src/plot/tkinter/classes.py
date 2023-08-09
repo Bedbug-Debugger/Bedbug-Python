2fb7ea881511e39f65ca4a029fc82af3f64dd797
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-09 12:23:25 +0330

    Minor change
    - Bound Up and Down keys.
    - Started writing variables for GUI management.

--- Codes/core/src/plot/tkinter/classes.py
+++ Codes/core/src/plot/tkinter/classes.py
@@ -7,12 +7,16 @@ class TkPlotterWindow:
 	def __init__(self, name: str):
 		self.name: str = name
 		self.tk_element = tk.Tk()
+		# Variables
+		self.num_of_signals: int = None
 		# Subwidgets
 		self.signal_name_frame = TkFrame(name='signal_name_frame', master=self.tk_element)
 		self.signal_plot_frame = TkPlotFrame(name='signal_plot_frame', master=self.tk_element)
 		# Event binding
 		self.tk_element.bind('<Key-Left>', self.handle_event)
 		self.tk_element.bind('<Key-Right>', self.handle_event)
+		self.tk_element.bind('<Key-Up>', self.handle_event)
+		self.tk_element.bind('<Key-Down>', self.handle_event)
 
 	def handle_event(self, event):
 		pass
