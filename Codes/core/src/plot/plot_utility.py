0035272cd551cc89ef3c20ef3a31b5616512e917
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-10 16:14:55 +0330

    Delete commented code
    It seems certain that the code won't be needed in the future. The new code has already passed its tests.

--- Codes/core/src/plot/plot_utility.py
+++ Codes/core/src/plot/plot_utility.py
@@ -33,34 +33,3 @@ def get_time_ticks(signals: list[tuple[str, str]]) -> list:
     time_ticks = list(time_ticks)
     time_ticks.sort()
     return time_ticks
-
-
-
-# def get_values_from_dict(time_value_dict: dict, num_of_time_ticks: int) -> list:
-#     """
-#     Return the list of values of a variable in all sampling time ticks, based on the variable's value dictionary.
-#     :param time_value_dict: the value dictionary of the variable, in the form of {time_tick: value}
-#     :param num_of_time_ticks: the number of time ticks until calling this function
-#     :return: list of values of the variable
-#     """
-#     # Initialize values
-#     values = [PlotConst.EMPTY_VALUE] * num_of_time_ticks
-#     # Fill registered values
-#     for time_index, value in time_value_dict.items():
-#         values[time_index] = value
-#     # Fill empty values with last value, if any
-#     value_exists_before = False
-#     first_index_with_value = -1
-#     for time_index in range(num_of_time_ticks):
-#         if values[time_index] == PlotConst.EMPTY_VALUE:
-#             if value_exists_before:
-#                 values[time_index] = values[time_index - 1]
-#         else:
-#             if not value_exists_before:
-#                 value_exists_before = True
-#                 first_index_with_value = time_index
-#     # Fill initial empty values with first value
-#     value_exists_before = False
-#     for time_index in range(first_index_with_value):
-#         values[time_index] = values[first_index_with_value]
-#     return values
