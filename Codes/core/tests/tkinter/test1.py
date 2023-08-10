92f1bc3a24dce51a34fff8233046e4f59e9265ed
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-10 15:17:03 +0330

    Run tests

--- Codes/core/tests/tkinter/test1.py
+++ Codes/core/tests/tkinter/test1.py
@@ -8,5 +8,17 @@ from ...src.plot import (
 def empty_window():
     tkp.open_window()
 
+def first_window():
+    a = 3
+    b = 2
+    c = 1
+    bd.add_data_multi({
+        'a': a,
+        'b': b,
+        'c': c
+    })
+    bd.plot()
+
 def run():
-    empty_window()
+    # empty_window()
+    first_window()
