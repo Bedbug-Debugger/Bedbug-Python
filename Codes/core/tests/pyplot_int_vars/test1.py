de855593555e0f12385af8ba1f3fff6f54ea2d08
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-10 20:25:52 +0330

    Run tests

--- Codes/core/tests/pyplot_int_vars/test1.py
+++ Codes/core/tests/pyplot_int_vars/test1.py
@@ -1,7 +1,4 @@
-from ...src import (
-    bedbug as bd,
-    const
-)
+from ...src import bedbug as bd
 
 
 def test_with_default_group_and_one_variable():
@@ -9,8 +6,8 @@ def test_with_default_group_and_one_variable():
     while index <= 10:
         index += 1
         bd.add_data("index", index)
-    print(bd.get_group(const.DEFAULT_GROUP_NAME).signals)
-    bd.plot()
+    print(bd.get_group(bd.const.DEFAULT_GROUP_NAME).signals)
+    bd.plot(gui_engine=bd.GuiEngine.PyPlot)
 
 
 def test_with_default_group_and_two_vars_and_pause():
@@ -19,7 +16,7 @@ def test_with_default_group_and_two_vars_and_pause():
     bd.add_data_multi({
         "a": a
     })
-    print(bd.get_group(const.DEFAULT_GROUP_NAME).signals)
+    print(bd.get_group(bd.const.DEFAULT_GROUP_NAME).signals)
     while a < 10:
         bd.add_data_multi({
             "a": a,
@@ -30,32 +27,35 @@ def test_with_default_group_and_two_vars_and_pause():
     while a < 20:
         bd.add_data("a", a)
         a += 3
-    print(bd.get_group(const.DEFAULT_GROUP_NAME).signals)
+    print(bd.get_group(bd.const.DEFAULT_GROUP_NAME).signals)
     bd.time.pause()
     a = 17
     bd.add_data("a", a)
     b = 19
     bd.add_data("b", b)
     bd.time.resume()
-    print(bd.get_group(const.DEFAULT_GROUP_NAME).signals)
+    print(bd.get_group(bd.const.DEFAULT_GROUP_NAME).signals)
     a = 23
     bd.add_data("a", a)
-    print(bd.get_group(const.DEFAULT_GROUP_NAME).signals)
-    bd.plot()
+    print(bd.get_group(bd.const.DEFAULT_GROUP_NAME).signals)
+    bd.plot(gui_engine=bd.GuiEngine.PyPlot)
 
 
 def test_with_multiple_groups_and_multiple_vars():
     a, b, c = 1, 2, 3
     G1 = bd.create_group("G1")
     G1.add_data("a", a)
+    print("Did G1\n")
     G2 = bd.create_group("G2")
     G2.add_data("b", b)
+    print("Did G2\n")
     G3 = bd.create_group("G3")
     G3.add_data("c", c)
-    bd.plot()
-    G2.plot()
+    print("Did G3\n")
+    bd.plot(gui_engine=bd.GuiEngine.PyPlot)
+    G2.plot(gui_engine=bd.GuiEngine.PyPlot)
 
 def run():
-    # test_with_default_group_and_one_variable()
+    test_with_default_group_and_one_variable()
     # test_with_default_group_and_two_vars_and_pause()
-    test_with_multiple_groups_and_multiple_vars()
+    # test_with_multiple_groups_and_multiple_vars()
--- Codes/test_run.py
+++ Codes/test_run.py
@@ -2,8 +2,8 @@ from core.tests.tests_import import *
 
 
 def main():
-    # pyplot_int_vars_test1.run()
-    tkinter_test1.run()
+    pyplot_int_vars_test1.run()
+    # tkinter_test1.run()
 
 
 if __name__ == '__main__':
