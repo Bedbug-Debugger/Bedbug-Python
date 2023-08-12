b5a999ebd332540aa9b7d5cf7384f2937ab1e2b3
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-12 18:48:49 +0330

    Run tests

--- Codes/core/tests/tkinter/test1.py
+++ Codes/core/tests/tkinter/test1.py
@@ -18,7 +18,20 @@ def first_window():
         'c': c
     })
     bd.plot()
+    print('Meow')
+
+def multi_tick():
+    a = 1
+    bd.add_data('a', a)
+    b = 2
+    bd.add_data('b', b)
+    c = 3
+    bd.add_data('c', c)
+    a = 4
+    bd.add_data('a', a)
+    bd.plot()
 
 def run():
     # empty_window()
-    first_window()
+    # first_window()
+    multi_tick()
