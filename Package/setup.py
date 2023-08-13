3d3a54f1f66d888d6cc2b272dbe8f95eb2ccc696
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-08-13 18:49:38 +0330

    Deploy bedbug-0.1.3

--- Package/setup.py
+++ Package/setup.py
@@ -2,17 +2,19 @@ from setuptools import setup
 
 setup(
 	name='bedbug',
-	version='0.1.2',
+	version='0.1.3',
 	description='A light-weight package for visual debugging',
 	url='https://github.com/Bedbug-Debugger/Bedbug-Python',
 	author='Ali Hadizadeh Moghadam, Ali Shayanpoor',
 	author_email='hadizadeh.ali@gmail.com, shayanpoor.ali66@gmail.com',
 	license='MIT',
-      
+    
+	package_dir={"": "src"},
+
 	install_requires=[
-        'matplotlib'
+		'matplotlib'
 	],
-	packages=['src/bedbug'],
+	packages=['bedbug'],
 	zip_safe=False,
     
 	include_package_data=True,
