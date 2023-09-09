fe6a46a76d58f4830a67046f18ddca7b9764adab
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-09-09 18:20:39 +0330

    Build 0.1.15

--- Package/setup.py
+++ Package/setup.py
@@ -5,7 +5,7 @@ from setuptools import (
 
 setup(
 	name='bedbug',
-	version='0.1.14',
+	version='0.1.15',
 	description='A light-weight package for visual debugging',
 	url='https://github.com/Bedbug-Debugger/Bedbug-Python',
 	author='Ali Hadizadeh Moghadam, Ali Shayanpoor',
